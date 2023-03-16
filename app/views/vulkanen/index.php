d3d76e45c21716a8c45cfa8c41d3b8798146188e
Waayway <thijs@vanwaaij.com>
2023-03-16 13:28:47 +0100

    Vulkaan.php view toegevoegd

---
+++ app/views/vulkanen/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+
+<body>
+    <div class="card">
+        <h1>Top 5 actiefste vulkanen ter wereld</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Naam</th>
+                    <th>Club</th>
+                    <th>Leeftijd</th>
+                    <th>Nationaliteit</th>
+                    <th>Salaris (miljoen)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($data["vulkanen"] as $row) {
+                ?>
+                    <tr>
+                        <td><?= $row->Naam ?></td>
+                        <td><?= $row->Club ?></td>
+                        <td><?= $row->Leeftijd ?></td>
+                        <td><?= $row->Nationaliteit ?></td>
+                        <td><?= $row->Salaris ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
